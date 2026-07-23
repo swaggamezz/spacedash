7971d9636e25e359704521864732cb5928daa0a6
swaggamezz <148389535+swaggamezz@users.noreply.github.com>
2026-07-23 14:47:57 +0200

    Add multi-feed watch center and historical search

---
+++ src/app/launch/[id]/watch/loading.tsx
@@ -0,0 +1,9 @@
+export default function WatchLoading() {
+  return (
+    <main className="watch-loading">
+      <span />
+      <strong>WATCH CENTER LADEN</strong>
+      <p>Videobronnen en missiestatus worden gecontroleerd…</p>
+    </main>
+  );
+}
